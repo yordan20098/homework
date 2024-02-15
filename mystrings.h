02cefbb9d684c91dca4df370c3b97e59f1feef61
yordan20098 <160110459+yordan20098@users.noreply.github.com>
2024-02-15 00:08:51 -0800

    Create mystrings.h

---
+++ mystrings.h
@@ -0,0 +1,8 @@
+#ifndef MYSTRINGS_H
+#define MYSTRINGS_H
+
+int strlength(const char *str);
+void strconcat(char *result,const char *str1,const char *str2);
+int strcompare(const char *str1,const char *str2);
+
+#endi
