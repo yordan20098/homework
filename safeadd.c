3fd8f74921a4c7a472874675fc0129a77e065345
yordan20098 <160110459+yordan20098@users.noreply.github.com>
2024-02-15 00:15:17 -0800

    Create safeadd.c

---
+++ safeadd.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "safeint.h"
+#include "util.h"
+
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        return 1;
+    }
+
+    int a = atoi(argv[1]);
+    int b = atoi(argv[2]);
+
+    struct SafeResult result = safeadd(a, b);
+
+    if (result.errorflag) {
+        printError("Overflow or underflow occurred.");
+        return 1;
+    }
+
+    printf("Sum: %d\n", result.value);
+
+    return 1;
+}
